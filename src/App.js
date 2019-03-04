5749f9af7387e5c2b87c629f4ebee0f7c2a59207
zep1994 <snaxbags@protonmail.com>
2019-03-04 12:00:32 -0600

    Remove Items

--- src/App.js
+++ src/App.js
@@ -18,24 +18,47 @@ const list = [
 }, ];
 
 class App extends Component {
-  render() {
-    return (
-     <div className="app">
-       {list.map(function(item) {
-         return (
-           <div key={item.objectID}>
-             <span>
-               <a href={item.url}>{item.title}</a>
-             </span>
-             <span>{item.author}</span>
-             <span>{item.num_comments}</span>
-             <span>{item.points}</span>
-           </div>
-         )
-       })}
-     </div>
-    );
-  }
-}
+  constructor(props) {
+    super(props) 
 
+    this.state = {
+      list,
+    }
+
+    this.onDismiss = this.onDismiss.bind(this);
+    }
+
+
+    onDismiss(id) {
+      const isNotId = item => item.objectID !== id;
+      const updatedList = this.state.list.filter(isNotId); 
+      this.setState({ list: updatedList });
+      }
+
+      render() {
+        return (
+          <div className="App">
+            {this.state.list.map(item =>
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span> 
+                <span>{item.num_comments}</span> 
+                <span>{item.points}</span> 
+                <span>
+                  <button
+                    onClick={() => this.onDismiss(item.objectID)}
+                    type="button"
+                    > Dismiss
+                  </button>
+                </span>
+              </div> 
+              )
+            }
+        </div> 
+      );
+    } 
+  }
+  
 export default App;
