26effb1ace6403c53306d1212285038209578df7
zep1994 <snaxbags@protonmail.com>
2019-03-04 16:54:46 -0600

    Add more button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import Search from './components/Search';
-import Table from './components/Table';
+import React, { Component } from 'react'
+import Search from './components/Search'
+import Table from './components/Table'
 import './App.css'
+import Button from './Button'
 
 const DEFAULT_QUERY = 'redux'
+const DEFAULT_HPP = '100'
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
+const PARAM_PAGE = 'page='
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
     }
 
@@ -25,16 +31,33 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result })
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : []
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ]
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    })
   }
 
   componentDidMount() {
     const { searchTerm } = this.state
+    this.setState({ searchKey: searchTerm })
     this.fetchSearchTopStories(searchTerm)
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error)
@@ -47,13 +70,14 @@ class App extends Component {
 
   onSearchSubmit(event) {
     const { searchTerm } = this.state
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm)
     event.preventDefault()
   }
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = this.state.result.hits.filter(isNotId)
     this.setState({
       result: { ...this.state.result, hits: updatedHits }
     });
@@ -61,6 +85,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state
+    const page = (result && result.page) || 0
 
     if (!result) { return null }
 
@@ -78,10 +103,13 @@ class App extends Component {
           <Table
             list={result.hits}
             onDismiss={this.onDismiss}
-
           />
         }
-
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
